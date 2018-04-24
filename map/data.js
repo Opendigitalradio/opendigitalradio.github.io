0f070e668e46d4e47a0f126278bf7dd8a4da393a
Stefan Pöschel <github@basicmaster.de>
2018-04-24 17:00:56 +0200

    Make DAB+ Bretzenheim/IRT-Radio-Test inactive

--- map/data.js
+++ map/data.js
@@ -4,22 +4,6 @@ var dataRegular = {
 		{
 			"type": "Feature",
 			"properties": {
-				"name": "DAB+ Bretzenheim",
-				"channel": "12A",
-				"location": "Bretzenheim, Kirchturm Maria Geburt",
-				"start": "05.08.2017",
-				"operator": "domradio Studio Nahe",
-				"operatorUrl": "http://www.studio-nahe.de/",
-				"moreInfoUrl": "https://www.facebook.com/digitalradiobretzenheim/"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [7.8661894, 49.8137324]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
 				"name": "RNT Associative",
 				"channel": "9A",
 				"location": "Montmartre, Paris, France",
@@ -537,6 +521,28 @@ var dataTest = {
 				"coordinates": [-0.766306, 51.254165]
 			}
 		},
+	]
+};
+
+var dataInactive = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "DAB+ Bretzenheim",
+				"channel": "12A",
+				"location": "Bretzenheim, Kirchturm Maria Geburt",
+				"start": "05.08.2017",
+				"operator": "domradio Studio Nahe",
+				"operatorUrl": "http://www.studio-nahe.de/",
+				"moreInfoUrl": "https://www.facebook.com/digitalradiobretzenheim/"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.8661894, 49.8137324]
+			}
+		},
 		{
 			"type": "Feature",
 			"properties": {
@@ -552,13 +558,7 @@ var dataTest = {
 				"type": "Point",
 				"coordinates": [11.55368, 48.14297]
 			}
-		}
-	]
-};
-
-var dataInactive = {
-	"type": "FeatureCollection",
-	"features": [
+		},
 		{
 			"type": "Feature",
 			"properties": {
